6351cf0385b029778915c0c0bb6c5fc7b09172bc
sunyongnan <839500300@qq.com>
2019-12-17 17:01:56 +0800

    fix bug
    change unzip

--- detect_jenkins_sdk.py
+++ detect_jenkins_sdk.py
@@ -15,26 +15,17 @@ from time import sleep
 def get_AgoraSDK(file_path,downlod_addres):
 
         local_addres = str(downlod_addres).split("/")[-1]
-
-        print(local_addres)
         urllib.request.urlretrieve(downlod_addres, file_path+local_addres)
         print(file_path+local_addres)
         """解压zip"""
         zip_file = zipfile.is_zipfile(file_path+local_addres)
         if zip_file:
-            fz = zipfile.ZipFile(file_path+local_addres, 'r')
-            for file in fz.namelist():
-                fz.extract(file, file_path)
-        else:
-            print('This is not zip')
-
-
-
-
+            os.system('unzip -n -d /Users/labtest/workspace_git/AgoraRTCEngine-test/AutoBuild/sdks {}'.format(
+                    file_path+local_addres))
 
 if __name__ == '__main__':
 
-    file_path = '/Users/labtest/workspace/AgoraRTCEngine-test/AutoBuild/sdks/'
+    file_path = '/Users/labtest/workspace_git/AgoraRTCEngine-test/AutoBuild/sdks/'
     """遍历清空文件夹"""
     for root, dirs, files in os.walk(file_path, topdown=False):
 

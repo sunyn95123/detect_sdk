bcd09c5ecaa0c8ba1ae4d3f64c44e1aa8ea96574
sunyongnan <839500300@qq.com>
2019-12-19 17:12:53 +0800

    update
    update

--- detect_jenkins_sdk.py
+++ detect_jenkins_sdk.py
@@ -15,11 +15,21 @@ def get_AgoraSDK(file_path,downlod_addres):
         local_addres = str(downlod_addres).split("/")[-1]
         urllib.request.urlretrieve(downlod_addres, file_path+local_addres)
         print(file_path+local_addres)
+        sleep(2)
         """解压zip"""
         zip_file = zipfile.is_zipfile(file_path+local_addres)
         if zip_file:
-            os.system('unzip -n -d /Users/labtest/workspace_git/AgoraRTCEngine-test/AutoBuild/sdks {}'.format(
-                    file_path+local_addres))
+        #     os.system('unzip -n -d /Users/labtest/workspace_git/AgoraRTCEngine-test/AutoBuild/sdks {}'.format(
+        #                 file_path+local_addres))
+            fz = zipfile.ZipFile(file_path + local_addres, 'r')
+            if 'SDK_for_Mac' in str(local_addres):
+                os.system('unzip -n -d /Users/labtest/workspace_git/AgoraRTCEngine-test/AutoBuild/sdks {}'.format(
+                    file_path + local_addres))
+            else:
+                for file in fz.namelist():
+                    fz.extract(file, file_path)
+                if 'SDK_for_SDK_for_Windows' in str(local_addres):
+                    sleep(5)
 
 if __name__ == '__main__':
 
